b3d79c40fcf3d7203a567b681d281cc2fcbcadb8
Diana <dianacanon2000@yahoo.es>
2022-12-08 03:21:27 -0500

    actualizacion 0812022

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,14 +1,41 @@
 import "./favorites.modules.css";
 import { NavLink } from "react-router-dom";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
+import { filterCards, orderCards } from "../../redux/actions/actions";
 import Card from "../Card/Card";
 
 const Favorites = (props) => {
+
+  const dispatch = useDispatch()
+
+  const handleClick = (e) => {
+    let { value, name } = e.target
+    if(name === 'order') {
+      dispatch(orderCards(value))
+    }
+    if(name === 'filter') {
+      dispatch(filterCards(value))
+    }
+  }
+
   return (
     <div className="container">
       <NavLink to="/home">
         <button className="buttonHome">Home</button>
       </NavLink>
+      <div>
+        <select name='order' onClick={handleClick}>
+          <option value='Ascendente'>Ascendente</option>
+          <option value='Descendente'>Descendente</option>
+        </select >
+        <hr/>
+        <select name='filter' onClick={handleClick}>
+        <option value='Male'>Male</option>
+        <option value='Female'>Female</option>
+        <option value='Genderless'>Genderless</option>
+        <option value='unknown'>unknown</option>
+        </select>
+      </div>
 
       {props.myFavorites?.map((eleFav, index) => (
             <Card
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,18 +1,47 @@
-import { ADD_FAVORITE, DELETE_FAVORITE } from "../actions/actions";
+import {
+  ADD_FAVORITE,
+  DELETE_FAVORITE,
+  FILTER,
+  ORDER,
+} from "../actions/actions";
 
 const initialState = {
   myFavorites: [],
+  allCharacters: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAVORITE:
-      return { ...state, myFavorites: [...state.myFavorites, action.payload] };
+      return {
+        ...state,
+        myFavorites: [...state.allCharacters, action.payload],
+        allCharacters: [...state.allCharacters, action.payload],
+      };
     case DELETE_FAVORITE:
       return {
         ...state,
-        myFavorites: state.myFavorites.filter((fav) => fav.id !== action.payload),
+        myFavorites: state.myFavorites.filter(
+          (fav) => fav.id !== action.payload
+        ),
       };
+    case FILTER:
+      const copyfilter = [...state.allCharacters];
+      const filterCharacters = copyfilter.filter(
+        (ele) => ele.gender === action.payload
+      );
+      return { ...state, myFavorites: filterCharacters };
+    case ORDER:
+      const copyOrder = [...state.allCharacters];
+
+      if (action.payload === "Ascendente") {
+        return { ...state, myFavorites: copyOrder.sort((a, b) => a.id - b.id) };
+      }
+      if (action.payload === "Descendente") {
+        return { ...state, myFavorites: copyOrder.sort((a, b) => b.id - a.id) };
+      } else {
+        return { ...state };
+      }
     default:
       return { ...state };
   }
