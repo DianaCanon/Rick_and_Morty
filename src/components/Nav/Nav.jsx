f890adc99bb090b89493ecfbf3cd15c382c2f943
Diana <dianacanon2000@yahoo.es>
2022-12-09 14:20:24 -0500

    actualizacion 09122022

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,29 +2,37 @@ import React from "react";
 /* import About from "../About/About"; */
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Nav.module.css";
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useLocation } from "react-router-dom";
 
 function Nav(props) {
   const handleSubmit = () => {
     props.logout();
   };
 
+  const location = useLocation();
+
   return (
     <>
       <div className={styles.divMenu}>
-        <NavLink to="/home"><span className={styles.checkbox}>Home</span></NavLink>
+        <NavLink to="/home">
+          <span className={styles.checkbox}>Home</span>
+        </NavLink>
         <NavLink to="/about">
           <span className={styles.checkbox}>About</span>
         </NavLink>
         <NavLink to="/">
-          <span onClick={handleSubmit} className={styles.checkbox}>Logout</span>
+          <span onClick={handleSubmit} className={styles.checkbox}>
+            Logout
+          </span>
         </NavLink>
-        <NavLink to='/favorites'><button className={styles.buttonFavorites}>Favoritos</button></NavLink>
-       </div>
+        <NavLink to="/favorites">
+          <button className={styles.buttonFavorites}>Favoritos</button>
+        </NavLink>
+      </div>
       <div className={styles.divSearch}>
-        <SearchBar onSearch={props.onSearch} />
-       
+        {location.pathname === "/home" && (
+          <SearchBar onSearch={props.onSearch} />
+        )}
       </div>
     </>
   );
